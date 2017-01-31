160c7e0482bf32e2809045ee46136331610531c5
David Moreno <dmoreno@coralbits.com>
2017-01-31 14:25:10 +0100

    Autosize the heatmap cells to use all possible space.

--- src/widget.js
+++ src/widget.js
@@ -4,7 +4,8 @@ const plugin_id="serverboards.plugin.serviceheatmap"
 const Model = React.createClass({
   getInitialState(){
     return {
-      services: store.getState().serverboard.serverboard.services
+      services: store.getState().serverboard.serverboard.services,
+      size: 20
     }
   },
   updateServices(){
@@ -14,33 +15,59 @@ const Model = React.createClass({
   },
   componentDidMount(){
     this.unsubscribe = store.subscribe( () => this.updateServices() )
+    // This is the initial size, but it will make last row unfilled, butcutted, same last column
+    let size = Math.floor( Math.sqrt(
+      (this.props.layout.width*this.props.layout.height*280*240) / this.state.services.length
+    ) )
+    let ok = false
+    let per_row, row_count, max_row_count
+    while( !ok ){
+      per_row = Math.floor( this.props.layout.width * 280 / size )
+      max_row_count = Math.floor( this.props.layout.height * 240 / size )
+      row_count = Math.ceil( this.state.services.length /  per_row )
+      console.log("max w", size, per_row, row_count, max_row_count)
+      ok = (row_count <= max_row_count )
+      if (!ok)
+        size-=1
+      if (size<20){
+        break;
+      }
+    }
+    this.setState({size: size - 2})
   },
   componentWillUnmount(){
     this.unsubscribe()
   },
   render(){
-    return <View services={this.state.services}/>
+    return <View services={this.state.services} size={this.state.size}/>
   }
 })
 
 function View(props){
+  const style = {
+    width: props.size,
+    minWidth: props.size,
+    height: props.size,
+    cursor: "pointer"
+  }
   return (
-    <div className="ui heatmap">
+    <div className="ui heatmap" style={{justifyContent:"center"}}>
       {props.services.map( (s) => (
         <span
           className={`cell ${Serverboards.utils.colorize(s.tags[0] || "")}`}
           title={`${s.name}: ${s.tags.join(', ')}`}
           data-tooltip={`${s.name}: ${s.tags.join(', ')}`}
           onClick={() => Serverboards.store.goto(`/services/${s.uuid}`)}
-          style={{cursor: "pointer"}}
+          style={style}
           />
       ) ) }
     </div>
   )
 }
 
-function main(el, config){
-  Serverboards.ReactDOM.render(<Model/>, el)
+function main(el, config, extra){
+  console.log(extra)
+  Serverboards.ReactDOM.render(<Model layout={extra.layout}/>, el)
 
   return function(){
     Serverboards.ReactDOM.unmountComponentAtNode(el)
