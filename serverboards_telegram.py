ce805e310b1d1d172fd0e017cd99b0ad8ce110c9
David Moreno <dmoreno@coralbits.com>
2017-02-08 16:44:02 +0100

    Improvements on telegram support
    * Do some long polling to allow more interactivity, but still has many
      edge cases
    * Included required serverboards bindings
    * Added cron refresh

--- serverboards_telegram.py
+++ serverboards_telegram.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__),'../bindings/python/'))
@@ -10,6 +10,15 @@ status=None
 def tg_url(method):
     return "https://api.telegram.org/bot%s/%s"%(settings["token"], method)
 
+def tg_get(cmd, query = None):
+    ret = requests.get(tg_url(cmd), params=query)
+    print(ret)
+    if ret.status_code == 200:
+        return ret.json()["result"]
+    else:
+        raise Exception(ret.json()["description"])
+
+
 def send_telegram_basic(chat_id, message):
     serverboards.rpc.debug("%d> %s"%(chat_id, message))
     data={
@@ -17,13 +26,12 @@ def send_telegram_basic(chat_id, message):
         "text" : message,
         "parse_mode" : "Markdown",
     }
-    ret = requests.post(tg_url("sendMessage"), data=data).json()
-    if not ret["ok"]:
-        raise Exception(ret["description"])
-    return ret["result"]
+    return tg_get("sendMessage", data)
 
 @serverboards.rpc_method
 def send_telegram(user=None, config=None, message=None):
+    ensure_has_config()
+    serverboards.debug(repr((status, config)))
     send_telegram_basic(
         status["code_to_chatid"][config["code"]],
         ("*%s*\n---\n%s"%(message['subject'], message["body"]))
@@ -31,78 +39,90 @@ def send_telegram(user=None, config=None, message=None):
 
 @serverboards.rpc_method
 def info():
-    return requests.get(tg_url("getMe")).json()["result"]
+    ensure_has_config()
+    return tg_get("getMe")
 
 @serverboards.rpc_method
-def updates(offset=None):
-    query={}
+def updates(offset=None, timeout=60):
+    query={
+        "timeout": timeout
+    }
 
     if offset:
         query["offset"]=offset
-    return requests.get(tg_url("getUpdates"), params=query).json()["result"]
+    return tg_get("getUpdates", query)
 
-def check_new_messages():
+@serverboards.rpc_method
+def refresh():
+    ensure_has_config()
+
     update=False
     code_to_chatid=status.get("code_to_chatid",{})
-    msgs=updates(status.get("lastid"))
+    msgs=updates(status.get("lastid"), timeout=59)
     for msg in msgs:
         serverboards.rpc.debug(msg)
         update=True
         if "message" in msg:
             chatid=msg["message"]["chat"]["id"]
+            text=msg["message"]["text"]
+            if text=="/start":
+                code_to_chatid={ k:v for k,v in code_to_chatid.items() if v!=chatid }
             if not chatid in code_to_chatid.values():
+                serverboards.rpc.debug("Creating a new code for chatid %s"%chatid)
                 code=''.join(random.choice("012345789ABCDEF") for x in range(6))
                 try:
                     send_telegram_basic(
                         chatid,
-                        "Welcome to the Serverboards Bot. Please use this code at your notification settings: *%s*"%code)
+                        "Welcome to the Serverboards Bot. Please use this code at your notification settings: *%s*"%code
+                        )
                 except:
                     import traceback; traceback.print_exc()
                 code_to_chatid[code]=chatid
-        status["lastid"]=max(status["lastid"], msg["update_id"]+1)
-    if not msgs:
-        serverboards.rpc.debug("No new messages")
+            else:
+                serverboards.rpc.info("%s< %s"%(chatid, text))
+                send_telegram_basic(
+                    chatid,
+                    "Sorry, I can only send notifications at the moment. If you talk to me I can do no more than ignore the message."
+                    )
+
+        status["lastid"]=max(status.get("lastid", 0), msg["update_id"]+1)
     status["code_to_chatid"]=code_to_chatid
+
+    if update:
+        serverboards.rpc.call("plugin.data_set", "status", status)
 
     return update
 
-serverboards.rpc.set_debug(sys.stderr)
+def ensure_has_config():
+    global settings, status
+    if not settings or not status:
+        settings=serverboards.rpc.call("settings.get", "serverboards.telegram/settings.telegram")
+        status=serverboards.rpc.call("plugin.data_get", "status")
 
 if __name__=="__main__":
     if len(sys.argv)==2 and sys.argv[1]=="test":
+        import settings
         settings={
-            "token" : "208790212:AAEI4qvKA__vDmPk_9kmkn4wtOsoE9LF4Yg"
+            "token" : settigs.TOKEN
         }
         status={
-            "lastid": 213604781,
-            "code_to_chatid": {
-                "297ECC": 239499457
-            }
+            "lastid": settigs.LAST_ID,
+            "code_to_chatid": settings.CODE_TO_CHATID
         }
 
-        check_new_messages()
-        print status
+        refresh()
+        print(status)
 
-        print send_telegram(
+        print(send_telegram(
             user={ "email": "dmoreno@serverboards.io" },
             config={
-                "code": "297ECC"
+                "code": "2B87D4"
             },
             message={
                 "subject":"This is a test message",
                 "body":"Body of the test message",
                 "extra":[]
-            })
+            }))
 
     else:
-        serverboards.rpc.debug("1")
-        settings=serverboards.rpc.call("settings.get", "serverboards.core.notifications/settings.telegram")
-        serverboards.rpc.debug("setting %s"%settings)
-        status=serverboards.rpc.call("plugin.data_get", "serverboards.core.notifications/telegram", "status")
-        serverboards.rpc.debug("status %s"%status)
-
-        if check_new_messages():
-            serverboards.rpc.call("plugin.data_set", "serverboards.core.notifications/telegram", "status", status)
-        serverboards.rpc.debug("3")
-
         serverboards.loop()
