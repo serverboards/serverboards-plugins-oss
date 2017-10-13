e75a381426661aa14797a9531b1a088356bf468d
David Moreno <dmoreno@coralbits.com>
2017-10-13 17:35:10 +0200

    Fix search for proper virtualenv

--- postinst.sh
+++ postinst.sh
@@ -2,5 +2,18 @@
 
 set -e
 
-virtualenv-3 env
+for venv in virtualenv-3 virtualenv-3.5 virtualenv-3.6; do
+  echo $venv
+  if which $venv 2>/dev/null; then
+    break
+  fi
+done
+
+
+if [ ! "$venv" ]; then
+  echo "Could not find virtual env 3"
+  exit 1
+fi
+
+$venv env
 env/bin/pip install -r requirements.txt
