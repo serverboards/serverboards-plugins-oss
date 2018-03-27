9eb77d22ad0e03a94f38c17dae37446bc66bf02f
David Moreno <dmoreno@coralbits.com>
2018-03-27 19:18:30 +0200

    PEP8

--- prometheus-init.py
+++ prometheus-init.py
@@ -1,30 +1,39 @@
 #!/usr/bin/python3
 
-import serverboards, sys, requests, time, json, urllib, os, shutil, yaml, sh, re
+import serverboards
+import sys
 import requests
+import time
+import os
+import yaml
+import sh
+import re
 from serverboards import rpc, action, print
-sys.stderr=serverboards.error
+sys.stderr = serverboards.error
 
-ssh=serverboards.Plugin("serverboards.core.ssh/daemon")
+ssh = serverboards.Plugin("serverboards.core.ssh/daemon")
+
 
 def cwd(*filenames):
     return os.path.join(os.environ["HOME"], *filenames)
 
+
 def http_open(host, port):
     try:
-        r = requests.head("http://%s:%s"%(host, port))
-        return True # something is back, no mather if its an error
-    except:
+        requests.head("http://%s:%s" % (host, port))
+        return True  # something is back, no mather if its an error
+    except Exception:
         return False
+
 
 def open_node_port(uuid, config, context={}):
     server = config.get("server")
-    hostname = config.get("hostname","localhost")
+    hostname = config.get("hostname", "localhost")
     port = int(config.get("port", "9100"))
     if not server:
         target = config.get("url")
         if target.startswith('http://'):
-            target=target[7:]
+            target = target[7:]
 
         if ':' in target:
             return target.split(':')[:2]
@@ -32,7 +41,8 @@ def open_node_port(uuid, config, context={}):
 
     # serverboards.debug("Opening SSH tunnel to %s:%s:%s"%(server, hostname, port), service_id=uuid, **context)
 
-    newport = ssh.open_port(service=server, hostname=hostname, port=port, context=context)
+    newport = ssh.open_port(
+        service=server, hostname=hostname, port=port, context=context)
 
     return ("localhost", newport)
 
@@ -40,7 +50,8 @@ def open_node_port(uuid, config, context={}):
 @serverboards.rpc_method
 def node_exporter_updated(service, update_promservices=True):
     config = service["config"]
-    context = dict( task="prometheus.node_exporter_update", service_id=service["uuid"] )
+    context = dict(task="prometheus.node_exporter_update",
+                   service_id=service["uuid"])
 
     # no remote SSH server, will not try to install
     if not config.get("server"):
@@ -48,11 +59,13 @@ def node_exporter_updated(service, update_promservices=True):
         update_promservices_yaml()
         return
 
-    (host, port) = open_node_port(service["uuid"], config, context={"task":"prometheus.node_exporter_updated"})
+    (host, port) = open_node_port(service["uuid"], config, context={
+        "task": "prometheus.node_exporter_updated"})
 
     # Already running, no install neede.
     if http_open(host, port):
-        print("Existing remote node_exporter, not reinstalling. (%s:%s)"%(host,port), **context)
+        print("Existing remote node_exporter, not reinstalling. (%s:%s)" %
+              (host, port), **context)
         update_promservices_yaml()
         return
 
@@ -68,24 +81,36 @@ def node_exporter_updated(service, update_promservices=True):
             toservice=server_uuid, tofile="/opt/serverboards/bin/",
             context=context)
         ssh.scp(
-            fromservice=None, fromfile=os.path.join(os.path.dirname(__file__), "node_exporter.service"),
+            fromservice=None,
+            fromfile=os.path.join(
+                os.path.dirname(__file__),
+                "node_exporter.service"),
             toservice=server_uuid, tofile="/etc/systemd/system/",
             context=context)
-        ssh.run(service=server_uuid, command="systemctl enable node_exporter", context=context)
-        ssh.run(service=server_uuid, command="systemctl start node_exporter", context=context)
-    except:
-        serverboards.error("Could not install prometheus node_exporter at the remote server.", **context)
+        ssh.run(service=server_uuid,
+                command="systemctl enable node_exporter", context=context)
+        ssh.run(service=server_uuid,
+                command="systemctl start node_exporter", context=context)
+    except Exception:
+        serverboards.error(
+            "Could not install prometheus node_exporter at the remote server.",
+            **context)
 
     if update_promservices:
         update_promservices_yaml()
 
-ASNI_TERM_CODES=re.compile("\033.*?m")
-class PrometheusOutputParse:
-    INFO="INFO"
-    WARN="ERRO"
-    ERRO="WARN"
 
-    EXTRA={"file":"prometheus/prometheus", "line":"--", "task": "prometheus.init"}
+ASNI_TERM_CODES = re.compile("\033.*?m")
+
+
+class PrometheusOutputParse:
+    INFO = "INFO"
+    WARN = "ERRO"
+    ERRO = "WARN"
+
+    EXTRA = {"file": "prometheus/prometheus",
+             "line": "--", "task": "prometheus.init"}
+
     def write(self, alldata, *args, **kwargs):
         alldata = alldata.decode('utf8')
         alldata = ASNI_TERM_CODES.sub("", alldata)
@@ -93,47 +118,58 @@ class PrometheusOutputParse:
             if not data:
                 continue
             if data.startswith(PrometheusOutputParse.INFO):
-                pid=data[5:9]
+                pid = data[5:9]
                 rpc.info(data[11:], pid=pid, **PrometheusOutputParse.EXTRA)
             elif data.startswith(PrometheusOutputParse.WARN):
-                pid=data[5:9]
+                pid = data[5:9]
                 rpc.warning(data[11:], pid=pid, **PrometheusOutputParse.EXTRA)
             elif data.startswith(PrometheusOutputParse.ERRO):
-                pid=data[5:9]
+                pid = data[5:9]
                 rpc.error(data[11:], pid=pid, **PrometheusOutputParse.EXTRA)
             else:
                 rpc.error(repr(data), **PrometheusOutputParse.EXTRA)
+
     def flush(*args, **kwargs):
         pass
 
+
 def prometheus_close_issue():
     action.trigger("serverboards.core.actions/close-issue",
-        issue="prometheus.norun"
-        )
+                   issue="prometheus.norun"
+                   )
+
+
 def prometheus_open_issue():
     action.trigger("serverboards.core.actions/open-issue",
-        issue="prometheus.norun",
-        title="Prometheus failed to start",
-        description="Trying to start prometheus failed. Check the logs."
-        )
+                   issue="prometheus.norun",
+                   title="Prometheus failed to start",
+                   description="Trying to start prometheus failed. " +
+                   "Check the logs."
+                   )
 
 
 @serverboards.rpc_method
 def start_prometheus():
-    serverboards.rpc.subscribe("service.updated[serverboards.prometheus/node_exporter]", node_exporter_updated)
-    serverboards.rpc.subscribe("service.created[serverboards.prometheus/node_exporter]", node_exporter_updated)
-    #serverboards.rpc.subscribe("service.updated[serverboards.prometheus/service]", node_exporter_updated)
-    context = dict( task="prometheus.init" )
+    serverboards.rpc.subscribe(
+        "service.updated[serverboards.prometheus/node_exporter]",
+        node_exporter_updated)
+    serverboards.rpc.subscribe(
+        "service.created[serverboards.prometheus/node_exporter]",
+        node_exporter_updated)
+    # serverboards.rpc.subscribe("service.updated[serverboards.prometheus/service]", node_exporter_updated)
+    context = dict(task="prometheus.init")
 
     # first update, maybe something changed when asleep
     try:
         update_promservices_yaml()
     except Exception as e:
-        import traceback; traceback.print_exc()
+        import traceback
+        traceback.print_exc()
         serverboards.error("Could not update promservices!", **context)
 
     if not os.path.exists(cwd("prometheus/prometheus")):
-        serverboards.error("Prometheus binary not available, trying to install", **context)
+        serverboards.error(
+            "Prometheus binary not available, trying to install", **context)
         from setup import setup
         setup()
         update_promservices_yaml()
@@ -144,45 +180,54 @@ def start_prometheus():
     if http_open("localhost", 9090):
         print("Prometheus already running, do nothing.", **context)
         prometheus_close_issue()
-        return 365*24*60*60 # restart in a year
+        return 365 * 24 * 60 * 60  # restart in a year
     else:
         print("Prometheus not running at localhost:9090", **context)
-
 
     try:
         prometheus = sh.Command(cwd("prometheus/prometheus"))
         prom = prometheus(
             "-web.listen-address", ":9090",
             "-config.file", cwd("prometheus.yml"),
-            _bg=True, _cwd=cwd("."), _out=PrometheusOutputParse(), _err_to_out=True,
-            )
+            _bg=True, _cwd=cwd("."), _out=PrometheusOutputParse(),
+            _err_to_out=True,
+        )
         time.sleep(3)
-        if os.waitpid(prom.pid, os.WNOHANG) != (0,0):
-            serverboards.error("Serverboards not running (3 sec wait).", **context)
+        if os.waitpid(prom.pid, os.WNOHANG) != (0, 0):
+            serverboards.error(
+                "Serverboards not running (3 sec wait).", **context)
             raise Exception("Could not start prometheus.")
         else:
             prometheus_close_issue()
     except ChildProcessError:
-        serverboards.error("Started but finished early. Check installation. Command was: %s: %s"%(cwd('.'), prom.ran), **context)
+        serverboards.error(
+            "Started but finished early. " +
+            "Check installation. Command was: %s: %s" % (
+                cwd('.'), prom.ran), **context)
         prometheus_open_issue()
-        return 30*60 # try in 30 min.
+        return 30 * 60  # try in 30 min.
     except Exception as e:
         import traceback
-        e=traceback.format_exc()
-        serverboards.error("Prometheus could not be started. Check installation. %s"%(e), **context)
+        e = traceback.format_exc()
+        serverboards.error(
+            "Prometheus could not be started. Check installation. %s" % (e),
+            **context)
         prometheus_open_issue()
-        return 30*60 # try in 30 min.
+        return 30 * 60  # try in 30 min.
 
-    return 365*24*60*60 # restart in a year
+    return 365 * 24 * 60 * 60  # restart in a year
+
 
 @serverboards.rpc_method
 def update_promservices_yaml():
-    context = dict( task="prometheus.update-yaml" )
+    context = dict(task="prometheus.update-yaml")
 
     serverboards.debug("Update the prometheus services yaml file", **context)
-    promservices=[]
-    services = serverboards.rpc.call("service.list", type="serverboards.prometheus/node_exporter")
-    services += serverboards.rpc.call("service.list", type="serverboards.prometheus/agent")
+    promservices = []
+    services = serverboards.rpc.call(
+        "service.list", type="serverboards.prometheus/node_exporter")
+    services += serverboards.rpc.call("service.list",
+                                      type="serverboards.prometheus/agent")
 
     for n, s in enumerate(services):
         try:
@@ -190,40 +235,44 @@ def update_promservices_yaml():
             if not config:
                 continue
             server = config.get("server")
-            (host, port)=open_node_port(s["uuid"], config, context=context)
-            target="%s:%s"%(host, port)
-            serverboards.debug("Added prometheus target at %s:%s via %s"%(host, port, server), service_id=s["uuid"], **context)
+            (host, port) = open_node_port(s["uuid"], config, context=context)
+            target = "%s:%s" % (host, port)
+            serverboards.debug("Added prometheus target at %s:%s via %s" % (
+                host, port, server), service_id=s["uuid"], **context)
 
             if not http_open(host, port) and s["type"] == "serverboards.prometheus/node_exporter":
-               # update node exporter, maybe install
+                # update node exporter, maybe install
                 node_exporter_updated(s, update_promservices=False)
 
-
-
             labels = {
-                "projects" : ','.join(s["projects"]),
+                "projects": ','.join(s["projects"]),
                 "instance": s["uuid"]
-                }
+            }
             tags = s["tags"]
             if tags:
                 labels["tags"] = ','.join(tags)
 
             promservices.append({
                 "targets": [target],
-                "labels" : labels
+                "labels": labels
             })
-        except:
-            import traceback; traceback.print_exc()
-            serverboards.error("Could not open port to service %s/%s."%(s["uuid"], s["name"]), service_id = s["uuid"], **context)
+        except Exception:
+            import traceback
+            traceback.print_exc()
+            serverboards.error("Could not open port to service %s/%s." %
+                               (s["uuid"], s["name"]), service_id=s["uuid"],
+                               **context)
 
-    with open(cwd("promservices.yaml"),"wt") as wd:
+    with open(cwd("promservices.yaml"), "wt") as wd:
         wd.write(yaml.dump(promservices))
-    serverboards.info("Updated prometheus service list with %d remote services"%(len(promservices)), **context)
+    serverboards.info(
+        "Updated prometheus service list with %d remote services"
+        % (len(promservices)), **context)
     return True
 
 
-if __name__=='__main__':
-    if len(sys.argv)==2 and sys.argv[1]=='test':
+if __name__ == '__main__':
+    if len(sys.argv) == 2 and sys.argv[1] == 'test':
         test()
     else:
         serverboards.loop()
--- serverboards-prometheus.py
+++ serverboards-prometheus.py
@@ -1,48 +1,60 @@
 #!/usr/bin/python3
 
-import serverboards, sys, requests, time, json, urllib
-from serverboards import rpc, print
-sys.stderr=serverboards.error
+import serverboards
+import sys
+import requests
+import time
+import json
+import urllib
+from serverboards import print
+sys.stderr = serverboards.error
 
-IGNORE_METRIC_NAMES=set(['instance','job'])
+IGNORE_METRIC_NAMES = set(['instance', 'job'])
 
-td_to_s_multiplier=[
+td_to_s_multiplier = [
     ("ms", 0.001),
     ("s", 1),
     ("m", 60),
-    ("h", 60*60),
-    ("d", 24*60*60),
+    ("h", 60 * 60),
+    ("d", 24 * 60 * 60),
 ]
 
-uuid_to_timer={}
+uuid_to_timer = {}
+
 
 def time_description_to_seconds(td):
     if type(td) in (int, float):
         return float(td)
     for sufix, multiplier in td_to_s_multiplier:
         if td.endswith(sufix):
-            return float(td[:-len(sufix)])*multiplier
+            return float(td[:-len(sufix)]) * multiplier
     return float(td)
+
 
 def decorate_serie(serie, name=None):
     """
     Returns the series decorated as Serverboards likes it, not as Prometheus
     returns it.
     """
-    metric = serie.get("metric",{})
-    name = name or metric.get("__name__",None)
+    metric = serie.get("metric", {})
+    name = name or metric.get("__name__", None)
     if not name:
-        name = ', '.join("%s: %s"%(k,v) for k,v in metric.items() if not k in IGNORE_METRIC_NAMES)
+        name = ', '.join("%s: %s" % (k, v)
+                         for k, v in metric.items()
+                         if k not in IGNORE_METRIC_NAMES)
     return {
         "name": name,
-        "values": serie.get("values",[])
+        "values": serie.get("values", [])
     }
 
-ssh=serverboards.Plugin("serverboards.core.ssh/daemon")
+
+ssh = serverboards.Plugin("serverboards.core.ssh/daemon")
+
+
 @serverboards.cache_ttl(3000)
 def port_tunnel(via, hostname, port):
     newport = ssh.open_port(service=via, hostname=hostname, port=port)
-    serverboards.debug("Opened new port: %s"% newport)
+    serverboards.debug("Opened new port: %s" % newport)
     return newport
 
 
@@ -59,27 +71,28 @@ def get(expression, service=None, start=None, end=None, step=None):
     url = service.get("config", {}).get("url", "http://localhost:9090")
     via = service.get("config", {}).get("via")
     if via:
-        url=urllib.parse.urlparse(url)
-        port=port_tunnel(via, url.hostname, url.port)
-        url="http://localhost:%d"%port
+        url = urllib.parse.urlparse(url)
+        port = port_tunnel(via, url.hostname, url.port)
+        url = "http://localhost:%d" % port
 
-    now=int(time.time())
+    now = int(time.time())
 
     if not start:
-        start=now-600
+        start = now - 600
     if not end:
-        end=now
+        end = now
     if not step:
-        step=14
+        step = 14
 
-    ret=[]
-    for expr in expression.split('\n'): # maybe several expresions, one per line
-        expr=expr.strip()
-        name=None
+    ret = []
+    # maybe several expresions, one per line
+    for expr in expression.split('\n'):
+        expr = expr.strip()
+        name = None
         if ':' in expr:
-            d=expr.split(':')
-            name=d[0]
-            expr=d[1].strip()
+            d = expr.split(':')
+            name = d[0]
+            expr = d[1].strip()
         if not expr:
             continue
         params = {
@@ -91,80 +104,87 @@ def get(expression, service=None, start=None, end=None, step=None):
         }
         # serverboards.debug("Get data from %s, %s: %s"%(url,repr(via), expr))
         try:
-            res = requests.get(url+"/api/v1/query_range", params=params)
-        except:
-            raise Exception("Coult not connect to the Prometheus server. Is it running?")
-        if res.status_code!=200:
+            res = requests.get(url + "/api/v1/query_range", params=params)
+        except Exception:
+            raise Exception(
+                "Coult not connect to the Prometheus server. Is it running?")
+        if res.status_code != 200:
             raise Exception(res.text)
 
         js = res.json()
-        if js.get("status")!="success":
+        if js.get("status") != "success":
             raise Exception("Unknown response from prometheus")
-        for x in js.get("data",{}).get("result",[]):
+        for x in js.get("data", {}).get("result", []):
             ret.append(decorate_serie(x, name=name))
     return ret
 
+
 def get_points(via=None, url=None, expression=None):
     if not url:
-        url="http://localhost:9090"
+        url = "http://localhost:9090"
     if via:
-        url=urllib.parse.urlparse(url)
-        port=port_tunnel(via, url.hostname, url.port)
-        url="http://localhost:%d"%port
-    now=int(time.time())
+        url = urllib.parse.urlparse(url)
+        port = port_tunnel(via, url.hostname, url.port)
+        url = "http://localhost:%d" % port
+    now = int(time.time())
     params = {
         "query": expression,
         "time": now,
         "_": now
     }
-    res = requests.get(url+"/api/v1/query", params=params)
+    res = requests.get(url + "/api/v1/query", params=params)
     return res.json()["data"]["result"]
+
 
 @serverboards.rpc_method
 def watch_start(id=None, period=None, service=None, expression=None, **kwargs):
     state = None
-    via=service.get("config", {}).get("via")
-    url=service.get("config", {}).get("url")
+    via = service.get("config", {}).get("via")
+    url = service.get("config", {}).get("url")
     period_s = time_description_to_seconds(period or "5m")
+    nstate = None
 
     def check_ok():
-        serverboards.debug("Checking expression: %s"%(expression))
+        serverboards.debug("Checking expression: %s" % (expression))
         p = get_points(via=via, url=url, expression=expression)
         if state != nstate:
-            serverboards.rpc.event("trigger", {"id":id, "value": p})
+            serverboards.rpc.event("trigger", {"id": id, "value": p})
         return True
 
     check_ok()
     timer_id = serverboards.rpc.add_timer(period_s, check_ok)
-    serverboards.info("Start Prometheus watch %s"%timer_id)
+    serverboards.info("Start Prometheus watch %s" % timer_id)
     return timer_id
+
 
 @serverboards.rpc_method
 def watch_stop(id):
-    serverboards.info("Stop Prometheus watch %s"%(id))
+    serverboards.info("Stop Prometheus watch %s" % (id))
     serverboards.rpc.remove_timer(id)
     return "ok"
+
 
 @serverboards.cache_ttl(30)
 def get_values(via=None, url=None):
     if not url:
-        url="http://localhost:9090"
+        url = "http://localhost:9090"
     if via:
-        url=urllib.parse.urlparse(url)
-        port=port_tunnel(via, url.hostname, url.port)
-        url="http://localhost:%d"%port
-    res = requests.get(url+"/api/v1/label/__name__/values")
+        url = urllib.parse.urlparse(url)
+        port = port_tunnel(via, url.hostname, url.port)
+        url = "http://localhost:%d" % port
+    res = requests.get(url + "/api/v1/label/__name__/values")
     return res.json()["data"]
+
 
 @serverboards.cache_ttl(30)
 def get_tags(via=None, url=None, value="", tag=None):
     if not url:
-        url="http://localhost:9090"
+        url = "http://localhost:9090"
     if via:
-        url=urllib.parse.urlparse(url)
-        port=port_tunnel(via, url.hostname, url.port)
-        url="http://localhost:%d"%port
-    res = requests.get(url+"/api/v1/series?match[]=%s"%value)
+        url = urllib.parse.urlparse(url)
+        port = port_tunnel(via, url.hostname, url.port)
+        url = "http://localhost:%d" % port
+    res = requests.get(url + "/api/v1/series?match[]=%s" % value)
     data = res.json()["data"]
     print("value", value, data)
     if not tag:
@@ -178,52 +198,58 @@ def get_tags(via=None, url=None, value="", tag=None):
             ret.add(d.get(tag))
         return [x for x in ret if x and not x.startswith('__')]
 
-BUILTINS = ["sum(","min(","max(","avg(","stddev(","stdvar(","count(","count_values(","bottomk(","topk(", "quantile("]
+
+BUILTINS = ["sum(", "min(", "max(", "avg(", "stddev(", "stdvar(",
+            "count(", "count_values(", "bottomk(", "topk(", "quantile("]
+
 
 @serverboards.rpc_method
 def autocomplete_values(current="", via=None, url=None, **kwargs):
     if not current:
         return []
     if '=' in current:
-        prefix,suffix = current.split('{')
+        prefix, suffix = current.split('{')
         tag, suffix = suffix.split('=')
         if suffix.startswith('"'):
             suffix = suffix[1:]
-        options = ['%s{%s="%s"}'%(prefix, tag, x)
-            for x in get_tags(via, url, prefix, tag)
-            if x.startswith(suffix)
-            ]
+        options = ['%s{%s="%s"}' % (prefix, tag, x)
+                   for x in get_tags(via, url, prefix, tag)
+                   if x.startswith(suffix)
+                   ]
     elif '{' in current:
-        prefix,suffix = current.split('{')
-        options = ['%s{%s="'%(prefix, x)
-            for x in get_tags(via, url, prefix)
-            if x.startswith(suffix)
-            ]
+        prefix, suffix = current.split('{')
+        options = ['%s{%s="' % (prefix, x)
+                   for x in get_tags(via, url, prefix)
+                   if x.startswith(suffix)
+                   ]
     else:
         options = get_values(via, url) + BUILTINS
-    for cpart in current.lower().replace('{','_').replace('=','_').split('_'):
+    for cpart in \
+            current.lower().replace('{', '_').replace('=', '_').split('_'):
         options = [x for x in options if cpart in x.lower()]
     return sorted(options)
+
 
 def connect_url_via_status(url, via):
     print("Check ", url, via)
     if via:
-        url=urllib.parse.urlparse(url)
+        url = urllib.parse.urlparse(url)
         print("Tunel to", url.hostname, url.port)
         try:
-            port=port_tunnel(via, url.hostname, url.port)
-        except:
+            port = port_tunnel(via, url.hostname, url.port)
+        except Exception:
             return "ssh-proxy-error"
-        url="http://localhost:%d"%port
+        url = "http://localhost:%d" % port
     print("Check url", url)
     try:
         res = requests.get(url)
     except Exception as e:
         return "down"
-    if res.status_code==200:
+    if res.status_code == 200:
         return "ok"
     else:
         return "nok"
+
 
 @serverboards.rpc_method
 def prometheus_is_up(service):
@@ -231,11 +257,13 @@ def prometheus_is_up(service):
         url=service["config"].get("url") or "http://localhost:9090",
         via=service["config"]["via"])
 
+
 @serverboards.rpc_method
 def agent_is_up(service):
     return connect_url_via_status(
         url=service["config"].get("url") or "http://localhost:9090",
         via=service["config"]["via"])
+
 
 @serverboards.rpc_method
 def node_exporter_is_up(service):
@@ -243,17 +271,19 @@ def node_exporter_is_up(service):
         url=service["config"].get("url") or "http://localhost:9100",
         via=service["config"]["server"])
 
-def test():
-    #res=get(expression="prometheus_rule_evaluation_failures_total")
-    #print(json.dumps(res, indent=2))
 
-    res=get_points(expression="up == 1")
+def test():
+    # res=get(expression="prometheus_rule_evaluation_failures_total")
+    # print(json.dumps(res, indent=2))
+
+    res = get_points(expression="up == 1")
     print(json.dumps(res, indent=2))
 
     print("Success")
 
-if __name__=='__main__':
-    if len(sys.argv)==2 and sys.argv[1]=='test':
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2 and sys.argv[1] == 'test':
         test()
     else:
         serverboards.loop()
