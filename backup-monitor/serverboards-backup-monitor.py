c506abb5cb8e7c19ab2023fec4750b031b1052dd
David Moreno <dmoreno@coralbits.com>
2018-10-19 18:53:26 +0200

    Added tests and adapted to async

--- backup-monitor/serverboards-backup-monitor.py
+++ backup-monitor/serverboards-backup-monitor.py
@@ -3,16 +3,17 @@
 import sys
 import datetime
 import hashlib
-import random
-import serverboards
-from serverboards import rpc
+import yaml
+import serverboards_aio as serverboards
+from serverboards_aio import rpc, print
+import curio
 
 
 plugin_id = "serverboards.backup.monitor"
 
 
 def convert_timespec_to_seconds(when):
-    serverboards.info("When %s" % repr(when))
+    print("When %s" % repr(when))
     if when == '12pm':
         return 12 * 60 * 60
     if when == '12am':
@@ -43,10 +44,11 @@ def get_next_when(when):
     """
     seconds_in_day = convert_timespec_to_seconds(when)
     today_seconds = time_in_seconds()
+    fullday = 24 * 60 * 60
     if seconds_in_day > today_seconds:
         return seconds_in_day - today_seconds
     else:
-        return today_seconds + (24 * 60 * 60) - seconds_in_day
+        return fullday - today_seconds + seconds_in_day
 
 
 def filename_template(filename):
@@ -78,70 +80,75 @@ class RemoteCheck:
         self.file_expression = file_expression
         self.when = when
 
-    def check(self):
-        if RemoteCheck.file_exist_timers.get(self.id):
-            rpc.remove_timer(RemoteCheck.file_exist_timers.get(self.id))
-
-        url = self.service["config"]["url"]
-
+    async def check(self):
         filename = filename_template(self.file_expression)
 
-        rpc.debug("Checking state")
-        res = rpc.call(
+        print("Checking state")
+        res = await rpc.call(
             "action.trigger_wait",
             "serverboards.core.ssh/exec",
-            dict(url=url, command="stat -c '%%s %%y' %s; stat -f -c '%%f %%s' %s" % (filename, filename)))
-        rpc.debug("SSH stat result: %s" % res)
+            dict(service=self.service, command="stat -c '%%y|%%s|%%n|' %s; stat -f -c '%%f|%%s' %s" % (filename, filename)))
+        print("SSH stat result: %s" % res)
+        maxdt = (datetime.datetime.now() - datetime.timedelta(days=1)).isoformat()
 
         exists = res.get('exit', -1) == 0
         if exists != self.prev_exists:
             if exists:
-                serverboards.rpc.event("trigger", id=self.id, state="exists")
-                res = res['stdout'].split()
+                res = res['stdout'].split('|')
+                mydatetime = res[0].split()
+                mydatetime = "%sT%s" % (mydatetime[0], ''.join(mydatetime[1:]))
+                state = "ok" if mydatetime < maxdt else "old"
+                if res[1] == '0':
+                    state = 'empty'
                 data = {
-                    "filename": filename,
-                    "size": int(res[0])/(1024*1024),  # In MB
-                    "datetime": res[1]+'T'+res[2],
-                    "disk_free": int(res[3])*int(res[4])/(1024*1024)
+                    "state": state,
+                    "ok": state == 'ok',
+                    "filename": res[2],
+                    "size": int(res[1])/(1024*1024),  # In MB
+                    "datetime": mydatetime,
+                    "disk_free": int(res[3])*int(res[4])/(1024*1024),
                 }
                 sha = hashlib.sha256(
-                    (self.file_expression + "-" + self.service["uuid"]).encode('utf8')
+                    (self.file_expression + "-" + self.service).encode('utf8')
                 ).hexdigest()
 
-                rpc.call("plugin.data.update", plugin_id, 'test-'+sha, data)
+                await rpc.event("trigger", id=self.id, **data)
+                await rpc.call("plugin.data.update", plugin_id, 'test-'+sha, data)
             else:
-                serverboards.rpc.event("trigger", {"id": self.id, "state": "not-exists"})
+                await rpc.event("trigger", {"id": self.id, "state": "not-exists"})
             self.prev_exists = exists
 
-        self.rearm()
+    async def loop(self):
+        while True:
+            await self.check()
+            next_when = get_next_when(self.when)
+            await serverboards.debug(
+                "Wait until %s: %02d:%02d:%02d (%d)" %
+                (self.when, next_when/(60*60), (next_when/60) % 60, next_when % 60, next_when))
+            await curio.sleep(next_when)
 
-    def rearm(self):
-        next_when = get_next_when(self.when)
-        serverboards.debug("Wait %d:%d:%d" %
-                           (next_when/(60*60), (next_when/60) % 60, next_when % 60))
-        timer_id = rpc.add_timer(next_when, self.check)
-        RemoteCheck.file_exist_timers[self.id] = timer_id
-
-    def start(self):
-        timer_id = rpc.add_timer(random.randint(0, 10), self.check)
-        RemoteCheck.file_exist_timers[self.id] = timer_id
+    async def start(self):
+        job = await curio.spawn(self.loop)
+        RemoteCheck.file_exist_timers[self.id] = job
 
 
 @serverboards.rpc_method
-def file_exists(id, service, file_expression, when):
-    RemoteCheck(id, service, file_expression, when).start()
-    serverboards.debug("Backup watcher ready for %s" % (file_expression))
+async def file_exists(id, service, file_expression, when):
+    rc = RemoteCheck(id, service, file_expression, when)
+    await rc.start()
+    await serverboards.debug("Backup watcher ready for %s" % (file_expression))
     return id
 
 
 @serverboards.rpc_method
 def stop_file_exists(id):
-    if RemoteCheck.file_exist_timers.get(id):
-        rpc.remove_timer(RemoteCheck.file_exist_timers.get(id))
+    job = RemoteCheck.file_exist_timers.get(id)
+    if job:
+        curio.cancel(job)
         del RemoteCheck.file_exist_timers[id]
 
 
-def test():
+async def test():
     assert convert_timespec_to_seconds("0am") == 0
     assert convert_timespec_to_seconds("12am") == 0
     assert convert_timespec_to_seconds("1am") == 60*60
@@ -152,23 +159,42 @@ def test():
     assert convert_timespec_to_seconds("24h") == 0
     assert convert_timespec_to_seconds("2h") == (2 * 60*60)
 
-    print("Convert")
-    print(convert_timespec_to_seconds("17:45") / (60.0*60.0))
-    print(convert_timespec_to_seconds("17:40") / (60.0*60.0))
-    print("Get Next")
-    print(get_next_when("1pm") / (60.0*60.0))
-    print(get_next_when("1am") / (60.0*60.0))
-    print(get_next_when("6pm") / (60.0*60.0))
-    print(get_next_when("17:45") / (60.0*60.0))
-    print(get_next_when("17:40") / (60.0*60.0))
-    print("Today secs")
+    await serverboards.debug("Convert")
+    await serverboards.debug("Now is %s" % datetime.datetime.now())
+    assert convert_timespec_to_seconds("17:45") == (17*60 + 45)*60
+    assert convert_timespec_to_seconds("17:40") == (17*60 + 40)*60
+
+    hour = 60*60
+    minute = 60
+    until_midnight = 8 * hour
+    await serverboards.debug("Get Next")
+    assert get_next_when("17:45") == hour + 45 * minute, (get_next_when("17:45"), hour + 45 * minute)
+    assert get_next_when("17:40") == hour + 40 * minute, (get_next_when("17:45"), hour + 40 * minute)
+    assert get_next_when("6pm") == 2 * hour, get_next_when("6pm")
+    assert get_next_when("23:59") == until_midnight - minute, (get_next_when("0am"), until_midnight - minute, 7 * hour + 59 * minute)
+    assert get_next_when("0am") == until_midnight, (get_next_when("0am"), until_midnight)
+    assert get_next_when("1am") == until_midnight + hour
+    assert get_next_when("1pm") == until_midnight + (13*60*60), (get_next_when("1pm"), until_midnight + (13*60*60))
+    await serverboards.debug("Today secs")
     print(time_in_seconds() / (60.0*60.0))
 
-    print(filename_template("test-{yesterday_}.tar.gz"))
+    await serverboards.debug("File template")
+    assert filename_template("test-{yesterday_}.tar.gz") == "test-20181018.tar.gz"
 
+    rc = RemoteCheck(1, "XXX", "/etc/passwd", "10am")
+    print(await rc.check())
+    rc = RemoteCheck(1, "YYY", "/etc/passwd", "10am")
+    print(await rc.check())
+
+    await file_exists(1, "XXX", "/etc/passwd", "10am")
+    await curio.sleep(3)
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'test':
-        test()
+        from freezegun import freeze_time
+        with freeze_time("2018-10-19T16:00:00"):
+            data = yaml.load(open('mock.yaml'))
+            serverboards.test_mode(test, data)
+            serverboards.loop()
     else:
         serverboards.loop()
