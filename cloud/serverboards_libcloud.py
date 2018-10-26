02c6088aad4dfda84f4898a2985122d080e982cb
David Moreno <dmoreno@coralbits.com>
2018-10-26 19:03:40 +0200

    PEP8

--- cloud/serverboards_libcloud.py
+++ cloud/serverboards_libcloud.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-import sys, os, uuid, time, re
-sys.path.append(os.path.join(os.path.dirname(__file__),'../bindings/python/'))
+import os
+import time
 import serverboards
-from serverboards import rpc, cache_ttl, print
+from serverboards import cache_ttl, print
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
 from libcloud.compute.types import NodeState
@@ -11,26 +11,27 @@ from libcloud.compute.types import NodeState
 GiB = 1024 * 1024 * 1024
 MiB = 1024 * 1024
 
-connections = {} # UUID of a service to a connection
+connections = {}  # UUID of a service to a connection
 
 state_trans = {
-  "terminated" : "stopped",
-  NodeState.ERROR: "error",
-  NodeState.RUNNING: "running",
-  NodeState.UNKNOWN: "unknown",
-  NodeState.PAUSED: "paused",
-  NodeState.STOPPED: "stopped",
-  NodeState.SUSPENDED: "suspended",
-  NodeState.REBOOTING: "rebooting",
-  NodeState.TERMINATED: "terminated",
+    "terminated": "stopped",
+    NodeState.ERROR: "error",
+    NodeState.RUNNING: "running",
+    NodeState.UNKNOWN: "unknown",
+    NodeState.PAUSED: "paused",
+    NodeState.STOPPED: "stopped",
+    NodeState.SUSPENDED: "suspended",
+    NodeState.REBOOTING: "rebooting",
+    NodeState.TERMINATED: "terminated",
 }
 
-PRIVATE_IP_MASKS=[
+PRIVATE_IP_MASKS = [
     "192.168.",
     "10.",
     "127.",
     "172.16."
 ]
+
 
 def is_public_ip(ip):
     for test in PRIVATE_IP_MASKS:
@@ -38,15 +39,17 @@ def is_public_ip(ip):
             return False
     return True
 
+
 def xml_to_json(root):
     ret = {}
     if root.attrib:
         ret.update(root.attrib)
     for child in root:
-        ret.update( xml_to_json(child) )
+        ret.update(xml_to_json(child))
     if not ret:
         ret = root.text
     return {root.tag: ret}
+
 
 def make_connection(config):
     type = config.get("type")
@@ -58,86 +61,87 @@ def make_connection(config):
         return AWSEC2(config)
     raise Exception("Could not create connexion to remote cloud provider")
 
+
 class Connection:
-  def __init__(self, type, driver):
-    self.driver = driver
-    self.type = type
+    def __init__(self, type, driver):
+        self.driver = driver
+        self.type = type
 
-  def describe(self, node):
-    return None
+    def describe(self, node):
+        return None
 
-  def guess_icon(self, node):
-    nodename=node.name.lower()
-    if 'debian' in nodename:
-        return 'debian.svg'
-    if 'fedora' in nodename:
-        return 'fedora.svg'
-    if 'ubuntu' in nodename:
-        return 'ubuntu.svg'
-    if 'linux' in nodename:
-        return 'linux'
-    return None
+    def guess_icon(self, node):
+        nodename = node.name.lower()
+        if 'debian' in nodename:
+            return 'debian.svg'
+        if 'fedora' in nodename:
+            return 'fedora.svg'
+        if 'ubuntu' in nodename:
+            return 'ubuntu.svg'
+        if 'linux' in nodename:
+            return 'linux'
+        return None
 
-  def get_node(self, uuid):
-      if hasattr(self.driver, "ex_get_node_by_uuid"):
-        return self.driver.ex_get_node_by_uuid(uuid)
-      else:
-        nodes = self.driver.list_nodes()
-        return next(x for x in nodes if x.uuid == uuid)
+    def get_node(self, uuid):
+        if hasattr(self.driver, "ex_get_node_by_uuid"):
+            return self.driver.ex_get_node_by_uuid(uuid)
+        else:
+            nodes = self.driver.list_nodes()
+            return next(x for x in nodes if x.uuid == uuid)
 
-  def details(self, node, extra_info=False):
-    extra = {
-        'private_ips':node.private_ips,
-        'public_ips':node.public_ips,
-        'mem_total':node.extra.get("used_memory"),
-    }
-    created_at = str(getattr(node, "created_at", None))
-    if created_at:
-        extra["created_at"] = created_at
-    if extra_info:
-        extra.update(node.extra)
-        extra["size"]=node.size
-        try:
-            extra["image"]=self.driver.get_image(extra["image_id"]).extra
-        except:
-            extra["image"]=node.image
+    def details(self, node, extra_info=False):
+        extra = {
+            'private_ips': node.private_ips,
+            'public_ips': node.public_ips,
+            'mem_total': node.extra.get("used_memory"),
+        }
+        created_at = str(getattr(node, "created_at", None))
+        if created_at:
+            extra["created_at"] = created_at
+        if extra_info:
+            extra.update(node.extra)
+            extra["size"] = node.size
+            try:
+                extra["image"] = self.driver.get_image(extra["image_id"]).extra
+            except:
+                extra["image"] = node.image
 
-        try:
-            extra["disk_total"] = extra["image"]["block_device_mapping"][0]["ebs"]["volume_size"] * 1024 # from GiB to MiB
-        except:
-            pass
+            try:
+                # from GiB to MiB
+                extra["disk_total"] = extra["image"]["block_device_mapping"][0]["ebs"]["volume_size"] * 1024
+            except:
+                pass
 
-        extra = ensure_jsonable(extra)
+            extra = ensure_jsonable(extra)
 
-    return {
-      "name" : node.name,
-      "description" : self.describe(node),
-      "id" : node.uuid,
-      "state" : state_trans.get(node.state, node.state),
-      "icon" : self.guess_icon(node),
-      "props" : extra
-    }
-
+        return {
+            "name": node.name,
+            "description": self.describe(node),
+            "id": node.uuid,
+            "state": state_trans.get(node.state, node.state),
+            "icon": self.guess_icon(node),
+            "props": extra
+        }
 
 
 class LibVirt(Connection):
     def __init__(self, config):
-      type = config.get("type")
-      cls=get_driver(Provider.LIBVIRT)
+        type = config.get("type")
+        cls = get_driver(Provider.LIBVIRT)
 
-      ssh_server = serverboards.service.get(config['server'])
-      keyfile=os.path.join(os.environ["HOME"],"../serverboards.core.ssh/id_rsa")
-      url=ssh_server["config"]['url']
-      if url.startswith('ssh://'):
-          url=url[6:]
-      url="qemu+ssh://%s/system?keyfile=%s"%(url, keyfile)
+        ssh_server = serverboards.service.get(config['server'])
+        keyfile = os.path.join(os.environ["HOME"], "../serverboards.core.ssh/id_rsa")
+        url = ssh_server["config"]['url']
+        if url.startswith('ssh://'):
+            url = url[6:]
+        url = "qemu+ssh://%s/system?keyfile=%s" % (url, keyfile)
 
-      #serverboards.debug("Connect to libvirt // %s"%url)
-      driver=cls( url )
-      super().__init__("libvirt", driver)
+        #serverboards.debug("Connect to libvirt // %s"%url)
+        driver = cls(url)
+        super().__init__("libvirt", driver)
 
-      self.prev_used_cpu_time_v={}
-      self.prev_used_cpu_time_t={}
+        self.prev_used_cpu_time_v = {}
+        self.prev_used_cpu_time_t = {}
 
     def details(self, node, extra_info=False):
         details = super().details(node, extra_info)
@@ -145,31 +149,31 @@ class LibVirt(Connection):
         if extra_info:
             extra = details["props"]
 
-            used_cpu_time=extra.get("used_cpu_time")
+            used_cpu_time = extra.get("used_cpu_time")
             if used_cpu_time:
-                used_cpu_time/=1000000000;
-                now=time.time()
+                used_cpu_time /= 1000000000
+                now = time.time()
                 if node.uuid in self.prev_used_cpu_time_v:
-                    extra["CPU_rt"]=(used_cpu_time-self.prev_used_cpu_time_v[node.uuid])/(now-self.prev_used_cpu_time_t[node.uuid])
-                self.prev_used_cpu_time_t[node.uuid]=now
-                self.prev_used_cpu_time_v[node.uuid]=used_cpu_time
-
+                    extra["CPU_rt"] = (used_cpu_time-self.prev_used_cpu_time_v[node.uuid]
+                                       )/(now-self.prev_used_cpu_time_t[node.uuid])
+                self.prev_used_cpu_time_t[node.uuid] = now
+                self.prev_used_cpu_time_v[node.uuid] = used_cpu_time
 
             lnode = self.get_libvirt_node(node)
-            if details["state"]=="running":
+            if details["state"] == "running":
                 extra["private_ips"], extra["public_ips"] = self.get_ips(lnode)
             extra["disk_total"], extra["disk_free_rt"] = self.get_disk_total_free(lnode)
 
             import xml.etree.ElementTree as ET
 
-            xmlo = ET.fromstring( lnode.XMLDesc() )
-            extra.update( xml_to_json( xmlo ) )
+            xmlo = ET.fromstring(lnode.XMLDesc())
+            extra.update(xml_to_json(xmlo))
 
-            if details["state"]=="running":
+            if details["state"] == "running":
                 try:
-                    graphics=extra["domain"]["devices"]["graphics"]
-                    if graphics["type"]=="spice":
-                        extra["spice_port"]=graphics["port"]
+                    graphics = extra["domain"]["devices"]["graphics"]
+                    if graphics["type"] == "spice":
+                        extra["spice_port"] = graphics["port"]
                 except:
                     # no graphics
                     pass
@@ -197,28 +201,31 @@ class LibVirt(Connection):
         except:
             return None, None
 
+
 class DigitalOcean(Connection):
     def __init__(self, config):
-      cls=get_driver(Provider.DIGITAL_OCEAN)
-      driver=cls(config['token'], api_version='v2')
-      super().__init__("digitalocean", driver)
+        cls = get_driver(Provider.DIGITAL_OCEAN)
+        driver = cls(config['token'], api_version='v2')
+        super().__init__("digitalocean", driver)
+
 
 class AWSEC2(Connection):
     def __init__(self, config):
-      cls=get_driver(Provider.EC2)
-      driver=cls(config["access_key"], config["access_secret"], region=config["region"])
-      super().__init__("aws.ec2", driver)
+        cls = get_driver(Provider.EC2)
+        driver = cls(config["access_key"], config["access_secret"], region=config["region"])
+        super().__init__("aws.ec2", driver)
 
 
 @cache_ttl(60)
 def get_connection(service):
-  conn = connections.get(service["uuid"], None)
-  if conn:
+    conn = connections.get(service["uuid"], None)
+    if conn:
+        return conn
+
+    conn = make_connection(service["config"])
+    connections[service["uuid"]] = conn
     return conn
 
-  conn = make_connection( service["config"] )
-  connections[service["uuid"]] = conn
-  return conn
 
 def ensure_jsonable(data):
     if isinstance(data, (str, int, float)):
@@ -226,7 +233,7 @@ def ensure_jsonable(data):
     if isinstance(data, list):
         return [ensure_jsonable(x) for x in data]
     if isinstance(data, dict):
-        return {k:ensure_jsonable(v) for k,v  in data.items()}
+        return {k: ensure_jsonable(v) for k, v in data.items()}
     try:
         return str(data)
     except:
@@ -235,46 +242,51 @@ def ensure_jsonable(data):
 
 @serverboards.rpc_method("details")
 def _details(service, vmc):
-  conn = get_connection(service)
-  return conn.details(conn.get_node(vmc), True)
+    conn = get_connection(service)
+    return conn.details(conn.get_node(vmc), True)
+
 
 @serverboards.rpc_method("list")
 def _list(service):
-  # print("Get list from libvirt", service)
-  conn = get_connection(service)
+    # print("Get list from libvirt", service)
+    conn = get_connection(service)
 
-  return [conn.details(node) for node in conn.driver.list_nodes()]
+    return [conn.details(node) for node in conn.driver.list_nodes()]
+
 
 @serverboards.rpc_method
 def start(service, vmc):
-  conn = get_connection(service)
-  try:
-    node = conn.get_node(vmc)
-    conn.driver.ex_start_node( node  )
-  except Exception as e:
-    if "already running" in str(e):
-      return False # already running
-  return True
+    conn = get_connection(service)
+    try:
+        node = conn.get_node(vmc)
+        conn.driver.ex_start_node(node)
+    except Exception as e:
+        if "already running" in str(e):
+            return False  # already running
+    return True
+
 
 @serverboards.rpc_method
-def stop(service, vmc, force = False):
-  conn = get_connection(service)
-  driver=conn.driver
-  if conn.type == 'libvirt':
-      n = driver.connection.lookupByUUIDString(vmc)
-      n.destroy()
-      return True
-  return driver.ex_stop_node( conn.get_node(vmc) )
+def stop(service, vmc, force=False):
+    conn = get_connection(service)
+    driver = conn.driver
+    if conn.type == 'libvirt':
+        n = driver.connection.lookupByUUIDString(vmc)
+        n.destroy()
+        return True
+    return driver.ex_stop_node(conn.get_node(vmc))
+
 
 @serverboards.rpc_method
 def pause(service, vmc):
-  conn = get_connection(service)
-  pass
-
-def printy( data ):
-  import yaml
-  print( yaml.dump( data ) )
+    conn = get_connection(service)
+    pass
 
 
-if __name__=='__main__':
-  serverboards.loop()
+def printy(data):
+    import yaml
+    print(yaml.dump(data))
+
+
+if __name__ == '__main__':
+    serverboards.loop()
--- cloud/serverboards_lxd.py
+++ cloud/serverboards_lxd.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 
-import sys, os, json
-sys.path.append(os.path.join(os.path.dirname(__file__),'../bindings/python/'))
+import json
 import serverboards
 from serverboards import rpc, Plugin, print
 
-PRIVATE_IP_MASKS=[
+PRIVATE_IP_MASKS = [
     "192.168.",
     "10.",
     "127.",
     "172.16."
 ]
 
+
 def is_public_ip(ip):
     for test in PRIVATE_IP_MASKS:
         if ip.startswith(test):
             return False
     return True
+
 
 def xpath(data, *q):
     if not q:
@@ -26,7 +27,7 @@ def xpath(data, *q):
     head = q[0]
     rest = q[1:]
     if head == "*":
-        print(type(data), type(data)==list, list, data)
+        print(type(data), type(data) == list, list, data)
         if type(data) == list:
             v = data
         else:
@@ -46,43 +47,48 @@ def xpath(data, *q):
 
 LOCALS = ["127.0", "fc00::"]
 
+
 def is_localhost(ip):
     for i in LOCALS:
         if ip.startswith(i):
             return True
     return False
 
+
 ssh = Plugin("serverboards.core.ssh/daemon")
+
 
 def maybe_sudo(service):
     return ["sudo"] if service["config"].get("sudo") else []
 
+
 def details_(node, extra=False):
-    extra_info={}
+    extra_info = {}
     if extra:
         ips = [
             ip
             for ip in xpath(node, "state", "network", "*", "addresses", "*", "address")
             if not is_localhost(ip)
-            ]
-        extra_info={
+        ]
+        extra_info = {
             "state": node["state"] or {},
             "config": {**node["config"], **node["expanded_config"]},
             "ephemeral": node.get("ephemeral"),
             'private_ips': [ip for ip in ips if not is_public_ip(ip)],
             'public_ips': [ip for ip in ips if is_public_ip(ip)],
-            }
+        }
 
     return {
         "name": node.get("name"),
         "state": (node.get("status") or "stopped").lower(),
-        "icon" : "cloud",
-        "description" : None,
-        "id" : node.get("name"),
-        "props" : {
+        "icon": "cloud",
+        "description": None,
+        "id": node.get("name"),
+        "props": {
             **extra_info
         }
     }
+
 
 @serverboards.rpc_method("list")
 def list_(service):
@@ -91,37 +97,41 @@ def list_(service):
     data = json.loads(json_data)
     ret = []
     for node in data:
-        ret.append( details_(node, extra=False) )
+        ret.append(details_(node, extra=False))
     return ret
+
 
 @serverboards.rpc_method
 def details(service, vmc):
     sudo = maybe_sudo(service)
-    json_data = ssh.run(service=service["config"]["server"], command=[*sudo,"lxc","list","--format=json",vmc])["stdout"]
+    json_data = ssh.run(service=service["config"]["server"], command=[*sudo, "lxc", "list", "--format=json", vmc])["stdout"]
     data = json.loads(json_data)[0]
 
     return details_(data, extra=True)
+
 
 @serverboards.rpc_method
 def start(service, vmc):
     sudo = maybe_sudo(service)
     try:
-        ssh.run(service=service["config"]["server"], command=[*sudo, "lxc", "start", vmc] )
+        ssh.run(service=service["config"]["server"], command=[*sudo, "lxc", "start", vmc])
     except:
         import traceback
         traceback.print_exc()
         return False
     return True
 
+
 @serverboards.rpc_method
 def stop(service, vmc, force=False):
     sudo = maybe_sudo(service)
     try:
-        ssh.run(service=service["config"]["server"], command=[*sudo, "lxc", "stop", vmc] )
+        ssh.run(service=service["config"]["server"], command=[*sudo, "lxc", "stop", vmc])
     except:
         import traceback
         traceback.print_exc()
         return False
     return True
+
 
 serverboards.loop()
