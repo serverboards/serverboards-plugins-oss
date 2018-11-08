91125d53fa52d5cff2ff27dea6ace0cd48429cb4
David Moreno <dmoreno@coralbits.com>
2018-11-08 15:46:50 +0100

    Fixes of rt updates and usability

--- prometheus/src/widget.js
+++ prometheus/src/widget.js
@@ -1,7 +1,7 @@
 const widget_id = "serverboards.prometheus/widget"
 const {React, moment, store, i18n, plugin} = Serverboards
 const {Error, Loading} = Serverboards.Components
-const {object_is_equal} = Serverboards.utils
+const {object_is_equal, map_get} = Serverboards.utils
 
 class Widget extends React.Component{
   constructor(props){
@@ -16,24 +16,40 @@ class Widget extends React.Component{
       loading: true,
       Lines: undefined,
     }
-    this.update = this.update.bind(this)
+    this.updateRangeStart = this.updateRangeStart.bind(this)
+    this.updateRangeEnd = this.updateRangeEnd.bind(this)
   }
   componentDidMount(){
     this.update()
-    store.on("project.daterange.start", this.update)
-    store.on("project.daterange.end", this.update)
+    store.on("project.daterange.start", this.updateRangeStart)
+    store.on("project.daterange.end", this.updateRangeEnd)
     plugin.load("serverboards.core.widgets/lines.js")
       .then(() => plugin.do_widget("serverboards.core.widgets/lines"))
       .then( widget => this.setState({"Lines": widget.component}) )
   }
   componentWillUnmount(){
-    store.off("project.daterange.start", this.update)
-    store.off("project.daterange.end", this.update)
+    store.off("project.daterange.start", this.updateRangeStart)
+    store.off("project.daterange.end", this.updateRangeEnd)
   }
-  update(data={}){
-    // console.log("Update", data, this.props)
-    const config = data.config || this.props.config
+  updateRangeStart(start){
+    this.setState({start}, this.update.bind(this))
+  }
+  updateRangeEnd(end){
+    this.setState({end}, this.update.bind(this))
+  }
+  update(props){
+    props = props || this.props
+    const config = props.config
     const {start, end} = this.state
+
+    // console.log("props", props)
+    // console.log("final config", config.expr)
+
+    if ((!config.expr) || (config.expr == {})){
+      // console.log("Empty expr")
+      this.setState({error: i18n("A PromQL expression is required"), loading: false})
+      return
+    }
 
     const params = {
       expression: config.expr,
@@ -47,7 +63,7 @@ class Widget extends React.Component{
           .then( (data) => {
       // console.log("Got data", data, params)
       if (data.length==0){
-        console.log("No data from %o", params)
+        // console.log("No data from %o", params)
         this.setState({error: i18n("No data received"), loading: false})
       }
       else{
@@ -59,16 +75,17 @@ class Widget extends React.Component{
           }
         }
 
-        this.setState({data: {"columns": ["","",""], "rows": dt}, loading: false})
+        this.setState({data: {"columns": ["","",""], "rows": dt}, loading: false, error: null})
       }
     }).catch( (error) => {
+      console.error("Error loading from %o: %o", params, error)
       this.setState({error, loading: false})
     })
   }
   componentWillReceiveProps(newprops){
     if (!object_is_equal(newprops.config, this.props.config)){
-      console.log("new props", newprops.config)
-      this.update({config: newprops})
+      // console.log("new props", newprops.config)
+      this.update(newprops)
     }
   }
   render(){
